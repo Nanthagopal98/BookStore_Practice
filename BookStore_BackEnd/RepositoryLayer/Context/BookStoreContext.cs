25a5bb8444ac232fc170631021f488df2582005d
unknown <s.nanthagopal1998@gmail.com>
2022-10-05 16:18:14 +0530

    [Nantha] - Project Setup and COnnected With Database using Entity Framework

---
+++ BookStore_BackEnd/RepositoryLayer/Context/BookStoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Context
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<UserEntity>Users { get; set; }
+    }
+}
